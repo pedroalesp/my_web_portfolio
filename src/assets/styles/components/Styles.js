77a6e0123015c05ab23464871672a55578ddc95f
Pedro Silva <pedroalejsp@gmail.com>
2021-12-25 23:47:48 -0300

    Responsive almost ready in Homepage

--- src/assets/styles/components/Styles.js
+++ src/assets/styles/components/Styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const breakpoints = {
-  phone: "320px",
-  tablet: "768px",
-  desk: "1024px",
+  phone: `(max-width: 320px)`,
+  big_phone: `(max-width: 480px)`,
+  tablet: `(max-width: 768px)`,
+  desk: `(max-width: 1024px)`,
 };
 
 export const Container = styled.div`
@@ -14,22 +15,48 @@ export const Container = styled.div`
 `;
 
 export const StyledHero = styled.section`
+  @media ${breakpoints.big_phone} {
+    padding: 4em 0 15px;
+  }
+  @media ${breakpoints.phone} {
+    padding: 4em 0 15px;
+  }
   width: fit-content;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  padding: 4em 0 0 0;
+  padding: 4em 3.8em 15px;
   border-bottom: 1.5px solid #fff;
   padding-bottom: 15px;
 
   h1 {
     font-size: 5em;
+
+    @media ${breakpoints.tablet} {
+      font-size: 4em;
+    }
+    @media ${breakpoints.phone} {
+      font-size: 3em;
+    }
+    @media ${breakpoints.big_phone} {
+      font-size: 3em;
+    }
   }
 
   h2 {
     font-family: "Montserrat";
     font-size: 2em;
+
+    @media ${breakpoints.tablet} {
+      font-size: 1.7em;
+    }
+    @media ${breakpoints.phone} {
+      font-size: 1.5em;
+    }
+    @media ${breakpoints.big_phone} {
+      font-size: 1.5em;
+    }
   }
 
   h3 {
@@ -50,6 +77,7 @@ export const HeroIcon = styled.div`
 `;
 
 export const StyledNav = styled.nav`
+
   &:before {
     content: "";
     display: block;
@@ -62,12 +90,20 @@ export const StyledNav = styled.nav`
   }
 
   display: flex;
-  margin-top: 3.5em;
-  margin-left: 1px;
+  margin: 3.5em 1px 0 1.5px;
   align-items: center;
   justify-content: center;
   flex-direction: row;
   min-height: 80px;
+
+  @media ${breakpoints.big_phone} {
+    flex-direction: column;
+    &:before {
+    top: calc(21.5em - 7px);
+    left: calc(50% - -5px);
+    height: calc(4em + -6px);
+    }
+  }
 
   ul {
     margin: 0;
@@ -78,6 +114,10 @@ export const StyledNav = styled.nav`
     border: 1.5px solid #fff;
     overflow: hidden;
     border-radius: 5px;
+
+    @media ${breakpoints.big_phone} {
+      flex-direction: column;
+    }
 
     li {
       &:first-child {
@@ -95,11 +135,27 @@ export const StyledNav = styled.nav`
       border-left: 1.5px solid #fff;
       padding: 7px 0;
 
+      @media ${breakpoints.big_phone} {
+        &:first-child {
+          border-top: none;
+          padding: 7px 10px;
+        }
+        &:nth-child(3) {
+          padding: 7px 16px;
+        }
+        &:nth-child(4) {
+          padding: 7px 1px;
+        }
+        border-left: 0px;
+        border-top: 1.5px solid #fff;
+      }
+
       a {
         color: #fff;
         text-decoration: none;
         cursor: pointer;
         padding: 0 1.4em 0 1.54em;
+        }
       }
     }
   }
