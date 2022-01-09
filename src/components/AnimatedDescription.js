6f2de399c476d30d49712671f5dfa7fd7802a297
Pedro Silva <pedroalejsp@gmail.com>
2022-01-09 11:32:33 -0300

    Fixed animation issues (retry2)

---
+++ src/components/AnimatedDescription.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { StyledAnimatedDescription } from "../assets/styles/components/Styles";
+
+const AnimatedDescription = () => (
+  <StyledAnimatedDescription>
+    <ul>
+      <li>Frontend Dev</li>
+      <li>Backend Dev</li>
+      <li>Copywriter</li>
+      <li>Jedi Knight</li>
+    </ul>
+  </StyledAnimatedDescription>
+);
+
+export default AnimatedDescription;
